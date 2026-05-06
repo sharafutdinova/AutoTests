12e4e6bb90c633436fc0a923840d4487410688c9
Шарафутдинова Алсу <sharafutdinova.a@autostels.ru>
2026-05-06 13:27:12 +0300

    debug pipeline

--- src/main/java/api/dao/AccountDao.java
+++ src/main/java/api/dao/AccountDao.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Builder
 @EqualsAndHashCode(callSuper = false)
 public class AccountDao {
-  private Long id;
+    private Long id;
   private String accountNumber;
   private Double balance;
   private Long customerId;
